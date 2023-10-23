c3154fc4ccd2b7729d0503a530ab2d624bc27211
mfranky78 <139232072+mfranky78@users.noreply.github.com>
2023-10-23 14:17:17 +0200

    CustumButton_Padding

--- lib/core/custom_button.dart
+++ lib/core/custom_button.dart
@@ -14,49 +14,52 @@ class CustomButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          GlassmorphicContainer(
-            width: 250,
-            height: 50,
-            borderRadius: 10,
-            blur: 1,
-            alignment: Alignment.bottomCenter,
-            border: 1,
-            linearGradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                const Color(0xFFffffff).withOpacity(0.9),
-                const Color.fromARGB(255, 128, 128, 128).withOpacity(0.1),
-              ],
-              stops: const [0.1, 1],
-            ),
-            borderGradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                const Color.fromARGB(255, 255, 255, 255).withOpacity(0.9),
-                const Color(0xFFFFFFFF).withOpacity(0.5),
-              ],
-            ),
-            
-            child: Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const SizedBox(width: 10),
-                  Icon(icon),            
-                  const SizedBox(width: 10),
-                  text,
+    return Padding(
+      padding: const EdgeInsets.all(3.0),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GlassmorphicContainer(
+              width: 250,
+              height: 50,
+              borderRadius: 10,
+              blur: 1,
+              alignment: Alignment.bottomCenter,
+              border: 1,
+              linearGradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  const Color(0xFFffffff).withOpacity(0.9),
+                  const Color.fromARGB(255, 128, 128, 128).withOpacity(0.1),
+                ],
+                stops: const [0.1, 1],
+              ),
+              borderGradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  const Color.fromARGB(255, 255, 255, 255).withOpacity(0.9),
+                  const Color(0xFFFFFFFF).withOpacity(0.5),
                 ],
               ),
+              
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(width: 10),
+                    Icon(icon),            
+                    const SizedBox(width: 10),
+                    text,
+                  ],
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
