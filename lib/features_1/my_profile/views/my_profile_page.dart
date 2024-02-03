3b52d50db745ea0eeb2ef0f8117598af92c16376
mfranky78 <139232072+mfranky78@users.noreply.github.com>
2024-02-03 12:10:53 +0100

    create my_profile provider

---
+++ lib/features_1/my_profile/views/my_profile_page.dart
@@ -0,0 +1,89 @@
+import 'package:famibo/features_1/my_profile/providers/user_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:famibo/core/backround_screen.dart';
+import 'package:famibo/core/custom_glasscontainer_flex.dart';
+import 'package:famibo/core/custom_glasscontainer_text.dart';
+import 'package:famibo/core/text_style_page.dart';
+
+class MyProfilePage extends ConsumerWidget {
+  const MyProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Hole den Zustand von deinem UserStateNotifier
+    final userState = ref.watch(userStateNotifierProvider);
+
+    // Lade Benutzerdaten, wenn das Widget gebaut wird
+    ref.read(userStateNotifierProvider.notifier).loadUserData();
+
+    return Scaffold(
+        body: Stack(children: [
+      BackgroundScreen(
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 32, 0, 0),
+          child: ContainerGlassFlex(
+            child: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back_sharp,
+                        size: 30,
+                      )),
+                  Text('My Profile', style: kTextHeadLine5),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, "/myprofilesetting");
+                      },
+                      child: const Icon(Icons.settings),
+                    ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 200,
+                        width: 250,
+                        child: userState.imageUrl != null &&
+                                userState.imageUrl!.isNotEmpty
+                            ? Image.network(userState.imageUrl!,
+                                fit: BoxFit.cover)
+                            : Image.asset('assets/images/dogchild.png'),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text('You are login with:', style: kTextHeadLine6),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      GlassContainerFixText(
+                          child:
+                              Text('${userState.name}', style: kTextHeadLine4)),
+                      GlassContainerFixText(
+                          child: Text('${userState.email}',
+                              style: kTextHeadLine4)),
+                      //  GlassContainerFixText(child: Text('Team: $team')),
+                    ],
+                  ),
+                ),
+              ),
+            ]),
+          ),
+        ),
+      )
+    ]));
+  }
+}
