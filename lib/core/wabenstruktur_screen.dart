4a1050007b1e8307f77d3a63f6f99f7d30aadcab
mfranky78 <139232072+mfranky78@users.noreply.github.com>
2024-02-19 18:24:21 +0100

    update code

--- lib/core/wabenstruktur_screen.dart
+++ lib/core/wabenstruktur_screen.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class WabenBackground extends StatelessWidget {
-  const WabenBackground({super.key});
+class HoneycombBackground extends StatelessWidget {
+  final Widget child;
+  const HoneycombBackground({super.key, required this.child});
 
   @override
   Widget build(BuildContext context) {
-    return SvgPicture.asset(
-      'assets/wabenstruktur.svg', // Pfad zu Ihrer SVG-Datei im Assets-Verzeichnis
-      semanticsLabel: 'Wabenstruktur',
-      // Optional: Passen Sie die Größe an, falls nötig
-      width: 200,
-      height: 200,
+    return Container(
+      color: const Color.fromARGB(211, 92, 92, 92),
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: SvgPicture.asset(
+              'assets/images/wabenstruktur.svg',
+              fit: BoxFit.cover, 
+            ),
+          ),
+          // Inhalt
+          child,
+        ],
+      ),
     );
   }
 }
