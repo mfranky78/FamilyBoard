528ac09eb0566c9664789cadff00704016262c64
mfranky78 <139232072+mfranky78@users.noreply.github.com>
2024-02-19 18:25:10 +0100

    change padding

--- lib/core/custom_glasscontainer_flex.dart
+++ lib/core/custom_glasscontainer_flex.dart
@@ -9,7 +9,7 @@ class ContainerGlassFlex extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.fromLTRB(20,54,20,20),
       child: GlassmorphicFlexContainer(
         
         borderRadius: 20,
@@ -18,7 +18,7 @@ class ContainerGlassFlex extends StatelessWidget {
           end: Alignment.bottomRight,
           colors: [
             const Color(0xFFffffff).withOpacity(0.06),
-            const Color(0xFFFFFFFF).withOpacity(0.03),
+            const Color(0xFFFFFFFF).withOpacity(0.06),
           ],
           stops: const [
             0.1,
@@ -26,13 +26,13 @@ class ContainerGlassFlex extends StatelessWidget {
           ],
         ),
         border: 2,
-        blur: 8,
+        blur: 2,
         borderGradient: LinearGradient(
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
           colors: [
-            const Color(0xFFffffff).withOpacity(0.3),
-            const Color(0xFFFFFFFF).withOpacity(0.3),
+            const Color(0xFFffffff).withOpacity(0.8),
+            const Color(0xFFFFFFFF).withOpacity(0.8),
           ],
         ),
         child: child, 
