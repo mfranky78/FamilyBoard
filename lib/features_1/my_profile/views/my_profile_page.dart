05d20a507935984ff866e89f460a6f93847d5b61
mfranky78 <139232072+mfranky78@users.noreply.github.com>
2024-03-21 15:46:41 +0100

    delete text and padding

--- lib/features_1/my_profile/views/my_profile_page.dart
+++ lib/features_1/my_profile/views/my_profile_page.dart
@@ -11,78 +11,74 @@ class MyProfilePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // Hole den Zustand von deinem UserStateNotifier
+    // UserStateNotifier
     final userState = ref.watch(userStateNotifierProvider);
 
-    // Lade Benutzerdaten, wenn das Widget gebaut wird
     ref.read(userStateNotifierProvider.notifier).loadUserData();
 
     return Scaffold(
         body: Stack(children: [
       HoneycombBackground(
-       child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 32, 0, 0),
-          child: ContainerGlassFlex(
-            child: Column(children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      icon: const Icon(
-                        Icons.arrow_back_sharp,
-                        size: 30,
-                      )),
-                  Text('My Profile', style: kTextHeadLine5),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, "/myprofilesetting");
-                      },
-                      child: const Icon(Icons.settings),
-                    ),
-                  )
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: 200,
-                        width: 250,
-                        child: userState.imageUrl != null &&
-                                userState.imageUrl!.isNotEmpty
-                            ? Image.network(userState.imageUrl!,
-                                fit: BoxFit.cover)
-                            : Image.asset('assets/images/dogchild.png'),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Text('You are login with:', style: kTextHeadLine6),
-                      const SizedBox(
-                        height: 40,
-                      ),
-                      GlassContainerFixText(
-                          child:
-                              Text('${userState.name}', style: kTextHeadLine4)),
-                      GlassContainerFixText(
-                          child: Text('${userState.email}',
-                              style: kTextHeadLine4)),
-                      //  GlassContainerFixText(child: Text('Team: $team')),
-                    ],
-                  ),
-                ),
-              ),
-            ]),
-          ),
-        ),
+       child: ContainerGlassFlex(
+         child: Column(children: [
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+             children: [
+               IconButton(
+                   onPressed: () {
+                     Navigator.of(context).pop();
+                   },
+                   icon: const Icon(
+                     Icons.arrow_back_sharp,
+                     size: 30,
+                   )),
+               Text('My Profile', style: kTextHeadLine5),
+               Padding(
+                 padding: const EdgeInsets.all(8.0),
+                 child: GestureDetector(
+                   onTap: () {
+                     Navigator.pushNamed(context, "/myprofilesetting");
+                   },
+                   child: const Icon(Icons.settings),
+                 ),
+               )
+             ],
+           ),
+           Padding(
+             padding: const EdgeInsets.all(16.0),
+             child: Center(
+               child: Column(
+                 crossAxisAlignment: CrossAxisAlignment.center,
+                 children: [
+                   SizedBox(
+                     height: 200,
+                     width: 250,
+                     child: userState.imageUrl != null &&
+                             userState.imageUrl!.isNotEmpty
+                         ? Image.network(userState.imageUrl!,
+                             fit: BoxFit.cover)
+                         : Image.asset('assets/images/dogchild.png'),
+                   ),
+                   const SizedBox(
+                     height: 20,
+                   ),
+                   Text('Du bist angemeldet:', style: kTextHeadLine6),
+                   const SizedBox(
+                     height: 40,
+                   ),
+                   GlassContainerFixText(
+                       child:
+                           Text('${userState.name}', style: kTextHeadLine4)),
+                   GlassContainerFixText(
+                       child: Text('${userState.email}',
+                           style: kTextHeadLine4)),
+                   //  GlassContainerFixText(child: Text('Team: $team')),
+                 ],
+               ),
+             ),
+           ),
+         ]),
+       ),
       )
     ]));
   }
