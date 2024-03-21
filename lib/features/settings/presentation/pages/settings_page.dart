1f2b688f56cd5fd099cb380eac05c16bf283c9aa
mfranky78 <139232072+mfranky78@users.noreply.github.com>
2024-03-21 15:45:53 +0100

    dlete padding

--- lib/features/settings/presentation/pages/settings_page.dart
+++ lib/features/settings/presentation/pages/settings_page.dart
@@ -17,84 +17,81 @@ class SettingsPage extends ConsumerWidget {
       body: Stack(
         children: [
           HoneycombBackground(
-           child: Padding(
-              padding: const EdgeInsets.fromLTRB(0,32,0,0),
-              child: ContainerGlassFlex(
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        icon: const Icon(
-                          Icons.arrow_back_sharp,
-                          size: 30,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 90,
-                      ),
-                      Text('Settings', style: kTextHeadLine5),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Image.asset('assets/images/team-settings.png'),
-                  ),
-                  const SizedBox(
-                    height: 50,
-                  ),
-                   Padding(
-                     padding: const EdgeInsets.fromLTRB(36,0,0,0),
-                     child: Text('Hier kannst du Allgemeine Einstellung machen', style: kTextHeadLine2),
+           child: ContainerGlassFlex(
+           child: Column(
+             children: [
+               Row(
+                 children: [
+                   IconButton(
+                     onPressed: () {
+                       Navigator.of(context).pop();
+                     },
+                     icon: const Icon(
+                       Icons.arrow_back_sharp,
+                       size: 30,
+                     ),
                    ),
-                  const SizedBox(
-                    height: 50,
-                  ),
-                  CustomButton(
-                      onTap: () {
-                        Navigator.pushNamed(context, "/language");
-                      },
-                      icon: Icons.language,
-                      text: const Text('Language')),
-                  CustomButton(
-                      onTap: () {
-                        Navigator.pushNamed(context, "/deleteaccount");
-                      },
-                      icon: Icons.delete_rounded,
-                      text: const Text('Account Löschen')),
-                      const SizedBox(height: 20,),
-                  Row(mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const GlassContainerFix(
-                        child: Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Center(
-                            child: Row(
-                              children: [
-                                Icon(Icons.dark_mode),
-                                SizedBox(width: 10,),
-                                Text('Dark Mode'),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 90,),
-                      Switch(
-                        value: isDarkMode,
-                        onChanged: (value) {
-                          ref.read(darkModeProvider.notifier).toggleDarkMode();
-                        },
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-                        ),
-            ))
+                   const SizedBox(
+                     width: 90,
+                   ),
+                   Text('Settings', style: kTextHeadLine5),
+                 ],
+               ),
+               Padding(
+                 padding: const EdgeInsets.all(16.0),
+                 child: Image.asset('assets/images/team-settings.png'),
+               ),
+               const SizedBox(
+                 height: 50,
+               ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(36,0,0,0),
+                  child: Text('Hier kannst du Allgemeine Einstellung machen', style: kTextHeadLine2),
+                ),
+               const SizedBox(
+                 height: 50,
+               ),
+               CustomButton(
+                   onTap: () {
+                     Navigator.pushNamed(context, "/language");
+                   },
+                   icon: Icons.language,
+                   text: const Text('Language')),
+               CustomButton(
+                   onTap: () {
+                     Navigator.pushNamed(context, "/deleteaccount");
+                   },
+                   icon: Icons.delete_rounded,
+                   text: const Text('Account Löschen')),
+                   const SizedBox(height: 20,),
+               Row(mainAxisAlignment: MainAxisAlignment.center,
+                 children: [
+                   const GlassContainerFix(
+                     child: Padding(
+                       padding: EdgeInsets.all(10.0),
+                       child: Center(
+                         child: Row(
+                           children: [
+                             Icon(Icons.dark_mode),
+                             SizedBox(width: 10,),
+                             Text('Dark Mode'),
+                           ],
+                         ),
+                       ),
+                     ),
+                   ),
+                   const SizedBox(width: 90,),
+                   Switch(
+                     value: isDarkMode,
+                     onChanged: (value) {
+                       ref.read(darkModeProvider.notifier).toggleDarkMode();
+                     },
+                   ),
+                 ],
+               ),
+             ],
+           ),
+                     ))
         ],
       ),
     );
